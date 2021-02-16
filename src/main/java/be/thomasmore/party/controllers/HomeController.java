176d53ccb92299d8a6966c0312d3e0ca9e3aac4b
GewoonToon <thundertonemail@gmail.com>
2021-02-16 11:18:39 +0100

    Added first dynamic page

--- src/main/java/be/thomasmore/party/controllers/HomeController.java
+++ src/main/java/be/thomasmore/party/controllers/HomeController.java
@@ -1,18 +1,25 @@
 package be.thomasmore.party.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    private int mySpecialNumber;
+
     @GetMapping({"/", "/home"})
-    public String home(){
+    public String home(Model model){
+        mySpecialNumber = (int) (Math.random()*11+0);
+        model.addAttribute("mySpecialNumber", mySpecialNumber);
         return "home";
     }
 
     @GetMapping("/about")
-    public String about(){
+    public String about(Model model){
+        mySpecialNumber = (int) (Math.random()*11+0);
+        model.addAttribute("mySpecialNumber", mySpecialNumber);
         return "about";
     }
 }
