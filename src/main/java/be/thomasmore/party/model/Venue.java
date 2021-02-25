a04831a46498963c9b864b7ca27e0e94050deaac
ToonDubois <60922276+ToonDubois@users.noreply.github.com>
2021-02-25 16:19:15 +0100

    Completed database bundle

--- src/main/java/be/thomasmore/party/model/Venue.java
+++ src/main/java/be/thomasmore/party/model/Venue.java
@@ -33,6 +33,10 @@ public class Venue {
         return capacity;
     }
 
+    public int getId() {
+        return id;
+    }
+
     public void setLinkmoreinfo(String linkmoreinfo) {
         this.link_more_info = linkmoreinfo;
     }
